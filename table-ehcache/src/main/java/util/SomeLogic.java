725ae115fa7c9f1f77d1c48181cbe177e982bbac
Daniele Bosetti <danielebosetti2@gmail.com>
2019-11-09 17:27:27 +0000

    mc

---
+++ table-ehcache/src/main/java/util/SomeLogic.java
@@ -0,0 +1,8 @@
+package util;
+
+public class SomeLogic {
+
+  public void go() {
+    throw new RuntimeException();
+  }
+}
---
+++ table-ehcache/src/test/java/test/SomeTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import util.SomeLogic;
+
+public class SomeTest {
+
+  @Test
+  public void someTest() {
+    SomeLogic o = new SomeLogic();
+    o.go();
+  }
+
+
+}
